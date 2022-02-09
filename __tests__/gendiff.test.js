4a0402c08741a46d25af8b1256cfc874e226e922
Boris <watasinonamaewa@gmail.com>
2022-02-09 23:07:12 +1000

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,67 +1,19 @@
 // import { buildDiffTree } from '../src/diffTree.js';
 // import stylish from '../src/formatters/stylish.js';
 // import { parseJson, parseYml } from '../src/parsers.js';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { cwd } from 'process';
 import gendiff from '../src/gendiff.js';
 
-const answer = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
+const testSheet = [
+  ['JSON', 'JSON', readFileSync(resolve(cwd('__fixtures__/answer'), '__fixtures__/answer'), 'utf8'), 'stylish'],
+  ['yml', 'yaml', readFileSync(resolve(cwd('__fixtures__/answer'), '__fixtures__/answer'), 'utf8'), 'stylish'],
+  ['yml', 'JSON', readFileSync(resolve(cwd('__fixtures__/answer_plain'), '__fixtures__/answer_plain'), 'utf8'), 'plain'],
+];
 
-const answerPlain = `Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`;
-
-test('gendiff', () => {
-  expect(gendiff('__fixtures__/file1.JSON', '__fixtures__/file2.JSON', 'stylish')).toBe(answer);
-  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yaml', 'stylish')).toBe(answer);
-  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.JSON', 'plain')).toBe(answerPlain);
+test.each(testSheet)('File formats - %p %p', (a, b, expected, formater) => {
+  const beforeFullPath = `${process.cwd()}/__fixtures__/file1.${a}`;
+  const afterFullPath = `${process.cwd()}/__fixtures__/file2.${b}`;
+  expect(gendiff(beforeFullPath, afterFullPath, formater)).toBe(expected);
 });
