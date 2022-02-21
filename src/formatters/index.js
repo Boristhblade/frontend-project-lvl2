618a8475ab8d8529f03e6f4ef8322c0682ffb7c4
Boris <watasinonamaewa@gmail.com>
2022-02-21 20:26:26 +1000

    Revert "fix formatters/index.js"
    This reverts commit b50e6d1e25e8b31de99588685c011076aff5d098.

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,12 +2,17 @@ import plain from './plain.js';
 import json from './json.js';
 import stylish from './stylish.js';
 
-const formaters = {
-  stylish,
-  plain,
-  json,
+const pickFormater = (formaterName) => {
+  switch (formaterName) {
+    case 'stylish':
+      return stylish;
+    case 'plain':
+      return plain;
+    case 'json':
+      return json;
+    default:
+      return stylish;
+  }
 };
-
-const pickFormater = (formaterName) => formaters[formaterName];
 
 export default pickFormater;
