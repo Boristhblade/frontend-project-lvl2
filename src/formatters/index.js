724a893a4fe7978f8c20ea142ab21b4145a9eac3
Boris <watasinonamaewa@gmail.com>
2022-02-24 23:38:31 +1000

    hotfix index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -4,10 +4,12 @@ import stylish from './stylish.js';
 
 const pickFormater = (formaterName, tree) => {
   switch (formaterName) {
+    case 'stylish':
+      return stylish(tree); // Если сделать объектом, как в парсерах,
     case 'plain':
-      return plain(tree);
+      return plain(tree); // Придется добавлять дефол кейс через тернарный оператор
     case 'json':
-      return json(tree);
+      return json(tree); // Так что мне кажется тут лучше свич
     default:
       return stylish(tree);
   }
